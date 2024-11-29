a886d745c6b1af16caf242686730bb3e9a7a3e1a
zhangxiang <zhangxiang@bestsec.cn>
2024-11-29 10:32:29 +0800

    自动导入插件增加自动导入样式的功能。

--- packages/utils/hony-ui-resolver.ts
+++ packages/utils/hony-ui-resolver.ts
@@ -3,10 +3,12 @@ import type {
   ComponentResolveResult,
 } from 'unplugin-vue-components';
 
+// 将 kebab-case 格式的字符串转换为 PascalCase 格式
 const toPascalCase = (kebabCaseName: string) =>
   kebabCaseName.replace(/-(\w)/g, (match, p1) => p1.toUpperCase());
 
-const toCamelCase = (name: string) => {
+// 将 kebab-case 格式的字符串转换为 camelCase 格式，如果不是则直接返回原字符串
+const toCamelCaseIfKebabCase = (name: string) => {
   // 如果是 kebab-case 格式 (带有短横线的命名)
   if (name.includes('-')) {
     return name
@@ -23,9 +25,24 @@ const toCamelCase = (name: string) => {
   return name;
 };
 
+// 将 PascalCase 格式的字符串转换为 kebab-case 格式，如果不是则直接返回原字符串
+const toKebabCaseIfPascalCase = (name: string) => {
+  // 判断是否为 PascalCase 格式（以大写字母开头）
+  if (/^[A-Z]/.test(name)) {
+    return name
+      .replace(/([a-z])([A-Z])/g, '$1-$2') // 小写字母后紧跟大写字母时加短横线
+      .replace(/([A-Z])([A-Z][a-z])/g, '$1-$2') // 连续大写后接小写时加短横线
+      .toLowerCase(); // 转为小写
+  }
+
+  // 如果不是 PascalCase，直接返回原字符串
+  return name;
+};
+
+// HonyUIResolver 函数，用于解析 Hony 组件
 export const HonyUIResolver = (): ComponentResolver => ({
   type: 'component',
-  resolve: (name: string): ComponentResolveResult => {
+  resolve: (name: string): ComponentResolveResult | null => {
     let ComponentName = name;
     // 处理以 Hony 开头的组件，例如 HonyButton -> hony-button
     if (name.startsWith('hony-')) {
@@ -33,12 +50,14 @@ export const HonyUIResolver = (): ComponentResolver => ({
       return {
         name: ComponentName,
         from: `hony-ui`,
+        sideEffects: `hony-ui/es/components/${name}/style/css.ts`,
       };
     }
     if (name.startsWith('Hony')) {
       return {
         name: ComponentName,
         from: `hony-ui`,
+        sideEffects: `hony-ui/es/components/${toKebabCaseIfPascalCase(name)}/style/css.ts`,
       };
     }
     return null;
